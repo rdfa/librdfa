fd91378406764fc8f1229f750977de7f5c036dfb
Dave Beckett <dave@dajobe.org>
2008-05-14 00:12:10 +0100

    Removed rdfa_update_base header

--- c/rdfa_utils.h
+++ c/rdfa_utils.h
@@ -314,7 +314,6 @@ char* rdfa_create_bnode(rdfacontext* context);
 
 // All functions that rdfa.c needs.
 void rdfa_update_uri_mappings(rdfacontext* context, const char* attr, const char* value);
-void rdfa_update_base(rdfacontext* context, const char* base);
 void rdfa_establish_new_subject(
    rdfacontext* context, const char* name, const char* about, const char* src,
    const char* resource, const char* href, const char* type_of);
