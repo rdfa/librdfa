d07ce1b8595048e7fe007e54174651d2033ab0b4
Dave Beckett <dave@dajobe.org>
2012-06-24 10:50:27 -0700

    Include config.h in other modules and remove raptor config include

--- c/rdfa.h
+++ c/rdfa.h
@@ -84,13 +84,6 @@
 #endif
 
 #ifdef LIBRDFA_IN_RAPTOR
-#ifdef HAVE_CONFIG_H
-#include <raptor_config.h>
-#endif
-
-#ifdef WIN32
-#include <win32_raptor_config.h>
-#endif
 #include "raptor2.h"
 #include "raptor_internal.h"
 #endif /* LIBRDFA_IN_RAPTOR */
